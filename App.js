42f553123bf841bff4ab979ca8f8c86625efe504
YewonSeo <yewonblairseo@gmail.com>
2020-11-10 15:18:49 +0100

    finished

--- App.js
+++ App.js
@@ -1,47 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React from "react";
 import { Alert } from "react-native";
-import Loading from './Loading';
-import Weather from './Weather';
+import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
+import { api_key } from "./api_key";
 
-const API_KEY = "1c2aec124cb5c997bd4067456321c4c9";
+const API_KEY = api_key;
 
 export default class extends React.Component {
-
   state = {
     isLoading: true
-  }
-
+  };
   getWeather = async (latitude, longitude) => {
-    const { data: {main: {temp}, weather }} = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
     this.setState({
       isLoading: false,
-      temp,
-      condition: weather[0].main
+      condition: weather[0].main,
+      temp
     });
-  }
-
-  getLocation = async() => {
+  };
+  getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
     } catch (error) {
-      Alert.alert("Can't find you..", "So sad, but you cannot use this app.");
+      Alert.alert("Can't find you.", "So sad");
     }
-  }
-
+  };
   componentDidMount() {
     this.getLocation();
   }
-
-  render(){
+  render() {
     const { isLoading, temp, condition } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />;
+    return isLoading ? (
+        <Loading />
+    ) : (
+        <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
-
 }
